5a950b0bd415ed5d8dd78f2ef5356aa4e73a4a33
arcodev <arcodev07@gmail.com>
2021-10-13 13:59:33 -0500

    Pasando children en los componentes

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import "./global.css";
 import Header from "./components/header/Header";
 import TopCardList from "./components/top-cards-list/Top-Cards-List";
 import Overview from "./components/overview/Overview";
+import Switch from "./components/switch/Switch";
 
 function App() {
   return (
     <>
-      <Header />
+    {/*Pasandole hijos al header */}
+      <Header>
+        <Switch />
+      </Header>
       <TopCardList />
       <Overview />
     </>
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,21 +1,21 @@
-import React from 'react'
-import './header.css'
-import Switch from '../switch/Switch'
+import React from "react";
+import "./header.css";
+import Switch from "../switch/Switch";
 
-function Header() {
-    return(
-        <header className="header">
-            <div className="wrapper">
-                <div className="header-grid">
-                    <div>
-                        <h1>Social Media Dashboard</h1>
-                        <p className="header-total">Total Followers: 23, 005</p>
-                    </div>
-                    <Switch />
-                </div>
-            </div>
-        </header>
-    )
+function Header({ children }) {
+  return (
+    <header className="header">
+      <div className="wrapper">
+        <div className="header-grid">
+          <div>
+            <h1>Social Media Dashboard</h1>
+            <p className="header-total">Total Followers: 23, 005</p>
+          </div>
+          {children}
+        </div>
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
